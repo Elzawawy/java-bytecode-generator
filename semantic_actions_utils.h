6105ab94089785c21da4c1e76845c46ce62e87b0
omar_swidan <oswidan97@gmail.com>
2020-06-03 05:02:56 +0200

    Add function to decalre a variable and another to check its existance

--- semantic_actions_utils.h
+++ semantic_actions_utils.h
@@ -16,5 +16,14 @@ namespace semantic_actions_util {
     unordered_map<string, pair<int, VarType>> varToVarIndexAndType;
     int currentVariableIndex = 1;
 
+    void declareVariable(string name, int varType) {
+        if(varToVarIndexAndType.count(name) == 1) 
+            throw std::logic_error("Variable Already Declared");
+        varToVarIndexAndType[name] = make_pair(currentVariableIndex++, static_cast<VarType>(varType));
+    }
+    bool checkIfVariableExists(string varName) {
+        return varToVarIndexAndType.count(varName);
+    }
+
 }
 #endif //SEMANTIC_ACTIONS_UTILS_H
