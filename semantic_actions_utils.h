2222440b99f6eb6a09c69c1ba3a3499ee43e2b7b
omar_swidan <oswidan97@gmail.com>
2020-06-03 05:02:21 +0200

    Add required variables for declaration

--- semantic_actions_utils.h
+++ semantic_actions_utils.h
@@ -5,7 +5,16 @@
 #ifndef SEMANTIC_ACTIONS_UTILS_H
 #define SEMANTIC_ACTIONS_UTILS_H
 
+#include <unordered_map>
+#include <string>
+#include <utility>
+
+using namespace std;
+
 namespace semantic_actions_util {
+    enum VarType{ INT_TYPE, FLOAT_TYPE };
+    unordered_map<string, pair<int, VarType>> varToVarIndexAndType;
+    int currentVariableIndex = 1;
 
 }
 #endif //SEMANTIC_ACTIONS_UTILS_H
